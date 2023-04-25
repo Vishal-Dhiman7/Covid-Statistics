83062961c57eab98a0184f5361c8d4f5f65284d5
Vishal-Dhiman7 <vishalnota@gmail.com>
2023-04-25 23:09:19 +0530

    first commit

---
+++ src/createDatabase.js
@@ -0,0 +1,8 @@
+const { connection } = require('./connector')
+const { data } = require('./data')
+
+const refreshAll = async () => {
+    await connection.deleteMany({})
+    await connection.insertMany(data);
+}
+refreshAll();
